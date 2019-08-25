3d08c90ec562a5c4e065ee07a8cadc87feff6378
gitrybrcoul <54250418+gitrybrcoul@users.noreply.github.com>
2019-08-25 16:38:13 +0200

    Add files via upload

---
+++ ppersonaldata .js
@@ -0,0 +1,5 @@
+Pronajimatel = " Pname Psurname, r.č. 121212/111 ";
+Najemce = "Nname Nsurname, r.č. 212121/222";
+accPron = "1111111111/1111";
+accNaj = "2222222222/2222";
+adresa = "Moje ulice 20, byt č. 10, 5555 Mesto ";

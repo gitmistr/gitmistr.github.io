f7f8bb7e5fd32f8ee4a9ecc82bedb08c89adabfe
rybrcoul-web <54250418+rybrcoul-web@users.noreply.github.com>
2019-08-26 22:36:40 +0200

    Delete ppersonaldata.js

--- ppersonaldata.js
+++
@@ -1,5 +0,0 @@
-Pronajimatel = "Pjmeno Pprijmeni, r.č. 5555555/111 ";
-Najemce = "Njmeno Nprijmeni, r.č. 212121/222";
-accPron = "1111111111/1111";
-accNaj = "2222222222/2222";
-adresa = "Moje ulice 20, byt č. 10, 5555 Mesto ";
